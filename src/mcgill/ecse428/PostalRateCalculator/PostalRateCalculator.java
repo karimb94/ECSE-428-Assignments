e9e89c56fb17e88236df27d6d9ad426cc4043dea
karimb.94 <karim.el-baba@mail.mcgill.ca>
2018-03-01 16:34:42 -0500

    Start of assignment B

---
+++ src/mcgill/ecse428/PostalRateCalculator/PostalRateCalculator.java
@@ -0,0 +1,9 @@
+package mcgill.ecse428.PostalRateCalculator;
+
+public class PostalRateCalculator {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/mcgill/ecse428/Tests/TDD.java
@@ -0,0 +1,13 @@
+package mcgill.ecse428.Tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TDD {
+
+	@Test
+	public void ValidInputsAndRate() {
+
+	}
+
+}
